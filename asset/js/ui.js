4a0c8d89873363a2be5d0b25221fd7216cbf707f
LKT <jnssanaee@nate.com>
2020-12-07 16:15:33 +0900

    Complete : WA 접근성 심사 보완사항

--- asset/js/ui.js
+++ asset/js/ui.js
@@ -1,5 +1,3 @@
-
-
 $(function() {
     $( "#header .btn_drawer, #nav .close" ).on( "click", function(e) {
         e.preventDefault();
@@ -9,5 +7,10 @@ $(function() {
     $( "#footer .btn_top" ).on( "click", function(e) {
         e.preventDefault();
         $('html, body').animate({scrollTop:0}, '300');
+        $('#header h1 a').focus();
     });
+
+    // iframe 영상에 title 넣기
+    const video_title = $('.area_tit .tit_detail').text() + ' 유튜브 영상';
+    $('.area_content iframe').prop('title', video_title);
 });
